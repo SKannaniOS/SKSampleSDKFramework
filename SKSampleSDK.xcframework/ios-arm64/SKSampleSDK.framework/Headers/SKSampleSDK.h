45ad0b4bacc23a879e0f8b0340d4069640a8a765
Satheesh Kannan <satheeshkannan.ios@gmail.com>
2022-11-23 12:27:14 +0530

    Framework added

---
+++ SKSampleSDK.xcframework/ios-arm64/SKSampleSDK.framework/Headers/SKSampleSDK.h
@@ -0,0 +1,18 @@
+//
+//  SKSampleSDK.h
+//  SKSampleSDK
+//
+//  Created by SK on 15/11/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SKSampleSDK.
+FOUNDATION_EXPORT double SKSampleSDKVersionNumber;
+
+//! Project version string for SKSampleSDK.
+FOUNDATION_EXPORT const unsigned char SKSampleSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SKSampleSDK/PublicHeader.h>
+
+
