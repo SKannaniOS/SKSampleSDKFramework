2f8467e0cdcb40a12ee7d7e8f4844c9e0da3e1a6
Satheesh Kannan <satheeshkannan.ios@gmail.com>
2022-11-28 14:27:28 +0530

    Package file edited

--- Package.swift
+++ Package.swift
@@ -2,15 +2,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "SK_SKSampleSDK",
+    name: "SK1_SKSampleSDK",
     platforms: [.iOS(.v11)],
     products: [
         .library(
-            name: "SK_SKSampleSDK",
+            name: "SK2_SKSampleSDK",
             targets: ["SKSampleSDK"]),
     ],
     targets: [
-        .target(name: "SK_SKSampleSDK"),
+        .target(name: "SK3_SKSampleSDK"),
         .binaryTarget(name: "SKSampleSDK", url: "https://github.com/SKannaniOS/SKSampleSDKFramework/raw/6013b64bfcfbfe79b996e28c543eb90472aa37dd/SKSampleSDK_iOS.xcframework.zip", checksum: "1041c73cc500fd1dbdc20c2a6be535345178f7e726d23bc1f9cb6926425995c0")
     ]
 )
