d6146dadad1373acb549f39836b542c74781f0d0
Satheesh Kannan <satheeshkannan.ios@gmail.com>
2022-11-28 14:02:27 +0530

    Package file edited

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
             targets: ["SKSampleSDK"]),
     ],
     targets: [
-        .binaryTarget(name: "SKSampleSDK", url: "https://github.com/SKannaniOS/SKSampleSDKFramework/raw/f51b21be504d9cc7ab82c71b26c2cd52bce4d193/SKSampleSDK.xcframework.zip", checksum: "1041c73cc500fd1dbdc20c2a6be535345178f7e726d23bc1f9cb6926425995c0")
+        .binaryTarget(name: "SKSampleSDK", url: "https://github.com/SKannaniOS/SKSampleSDKFramework/raw/6013b64bfcfbfe79b996e28c543eb90472aa37dd/SKSampleSDK_iOS.xcframework.zip", checksum: "1041c73cc500fd1dbdc20c2a6be535345178f7e726d23bc1f9cb6926425995c0")
     ]
 )
